48abcd814279dd4a99a6d56377cb2785b802d696
Matthew Wakefield <matthew.wakefield@wehi.edu.au>
2020-01-22 21:41:07 +1100

    add regex based functions for extracting tags from raw alignment bytestrings bugfixes and working reference and position parsers with ref_id to reference name dictionary created on class init

--- pylazybam/bam.py
+++ pylazybam/bam.py
@@ -16,60 +16,143 @@ from pylazybam.bgzf import BgzfWriter as FileWriter
 
 # Parsing functions
 
-def get_flag_as_int(alignment):
+def get_flag(alignment):
     return struct.unpack('<H',alignment[18:20])[0]
 
-def get_read_name_length(alignment):
+def get_len_read_name(alignment):
     return struct.unpack('<B',alignment[12:13])[0]
 
-def get_read_name_text(alignment, read_name_length):
+def get_read_name(alignment, read_name_length):
     return alignment[36:36+read_name_length].decode("utf-8")
 
-def get_read_name_raw(alignment, read_name_length):
+def get_raw_read_name(alignment, read_name_length):
     return alignment[36:36+read_name_length]
 
-def get_number_of_cigar_operations(alignment):
+def get_n_cigar_op(alignment):
     return struct.unpack('<H',alignment[16:18])[0]
 
-def get_sequence_length(alignment):
+def get_len_sequence(alignment):
     return struct.unpack('<i',alignment[20:24])[0]
 
-def get_tag_raw(alignment, len_read_name, n_cigar_op, len_sequence):
+def get_tag_bytes(alignment, len_read_name, n_cigar_op, len_sequence):
     start = 36 + len_read_name + (4*n_cigar_op) +  (len_sequence+1)//2 + len_sequence
     return alignment[start:]
 
-def get_sequence_raw(alignment, len_read_name, n_cigar_op, len_sequence):
+def get_raw_seq(alignment, len_read_name, n_cigar_op, len_sequence):
     start = 36 + len_read_name + (4*n_cigar_op)
     end = start + (len_sequence+1)//2
     return alignment[start:end+1]
 
-def get_ref(alignment):
-    return
+def get_raw_cigar(alignment,len_read_name, n_cigar_op):
+    start = 36 + len_read_name
+    end = 36 + len_read_name + (4*n_cigar_op)
+    return alignment[start:end+1]
+
+def get_ref_index(alignment):
+    return struct.unpack('<i', alignment[4:8])[0]
 
 def get_pos(alignment):
-    return
+    return struct.unpack('<i', alignment[8:12])[0]
 
-def quick_AS_XS(tag_raw):
-    if (tag_raw[4:7] == b'XSC' or tag_raw[4:7] == b'ZSC') and tag_raw[:3] == b'ASC':
-        return (struct.unpack('<B',tag_raw[3:4])[0],struct.unpack('<B',tag_raw[8:9])[0])
+def get_mapq(alignment):
+    return struct.unpack('<B', alignment[13:14])[0]
+
+def get_AS(tag_bytes):
+    """A function to extract an AS tag from a raw bam byte string
+    Can be used on a raw alignment entry.
+    Raises ValueError if multiple matches.
+    Recommended try accept for use on raw alignment with fall back
+    to calling on only the tag byte string.
+    """
+    match = re.findall(b'ASC.',tag_bytes)
+    if not match:
+        return None
+    elif len(match) != 1:
+        raise ValueError(f"More than one match to b'ASC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
     else:
-        raise TypeError
+        return struct.unpack('<xxxB',match[0])[-1]
 
-def decode_sequence(seq_raw):
+def get_AS(tag_bytes):
+    """A function to extract an AS tag from a raw bam byte string
+    Can be used on a raw alignment entry.
+    Raises ValueError if multiple matches.
+    Recommended try accept for use on raw alignment with fall back
+    to calling on only the tag byte string.
+    """
+    match = re.findall(b'ASC.',tag_bytes)
+    if not match:
+        return None
+    elif len(match) != 1:
+        raise ValueError(f"More than one match to b'ASC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
+    else:
+        return struct.unpack('<xxxB',match[0])[-1]
+
+def get_XS(tag_bytes):
+    """A function to extract an XS tag from a raw bam byte string
+    for genome aligner definition of XS where XS:i:<int> is the
+    alignment score of the next best alignment.
+    This is not the same as the spliced aligner XS tag that represents
+    the strand on which the intron occurs
+    Can be used on a raw alignment entry.
+    Raises ValueError if multiple matches.
+    Recommended try accept for use on raw alignment with fall back
+    to calling on only the tag byte string.
+    """
+    match = re.findall(b'XSC.',tag_bytes)
+    if not match:
+        return None
+    elif len(match) != 1:
+        raise ValueError(f"More than one match to b'XSC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
+    else:
+        return struct.unpack('<xxxB',match[0])[-1]
+
+def get_ZS(tag_bytes):
+    """A function to extract an ZS tag from a raw bam byte string
+    ZS is the equivalent to XS:i:<int> tag in some spliced aligners
+    including HISAT2.
+    Can be used on a raw alignment entry.
+    Raises ValueError if multiple matches.
+    Recommended try accept for use on raw alignment with fall back
+    to calling on only the tag byte string.
+    """
+    match = re.findall(b'ZSC.',tag_bytes)
+    if not match:
+        return None
+    elif len(match) != 1:
+        raise ValueError(f"More than one match to b'ZSC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
+    else:
+        return struct.unpack('<xxxB',match[0])[-1]
+
+def get_MD(tag_bytes):
+    """A function to extract an MD tag from a raw bam byte string
+    Can be used on a raw alignment entry.
+    Raises ValueError if multiple matches.
+    Recommended try accept for use on raw alignment with fall back
+    to calling on only the tag byte string.
+    """
+    match = re.findall(b'MDZ[0-9ACGTN^]+\x00',tag_bytes)
+    if not match:
+        return None
+    elif len(match) != 1:
+        raise ValueError(f"More than one match to b'MDZ[0-9ACGTN^]+\x00' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
+    else:
+        return match[0][3:-1].decode()
+
+def decode_bam_seq(bamseq):
     bam_bases = '=ACMGRSVTWYHKDBN'
     result = ''
-    for x in array('B', seq_raw):
+    for x in array('B', bamseq):
         result += bam_bases[x >> 4]
         result += bam_bases[x & 0b1111]
     return result
-    
-def get_sequence_text(alignment, len_read_name, n_cigar_op, len_sequence):
-    return decode_sequences(get_sequence_raw(alignment,
-                                             len_read_name,
-                                             n_cigar_op,
-                                             len_sequence)
 
-class Reader():
+def decode_cigar(raw_cigar):
+    codes = 'MIDNSHP=X'
+    cigar = [str(x >> 4) + codes[x & 0b1111] for x in array('I', raw_cigar)]
+    return ''.join(cigar)
+   
+
+class FileReader():
     """A Pure Python Lazy Bam Parser Class
     """
     def __init__(self, ubam):
@@ -78,6 +161,9 @@ class Reader():
         assert self.magic == b'BAM\x01' ###TODO add more informative errors
         self.raw_header, self.ascii_header = self.read_header()
         self.raw_refs, self.refs = self.read_refs()
+        self.index_to_ref = dict(zip(range(len(self.refs)),self.refs.keys()))
+        self.ref_to_index = dict(zip(self.refs.keys(),range(len(self.refs))))
+        self.index_to_ref[-1] = '*'
         self.alignments = self._get_alignments()
 
     def __enter__(self):
@@ -112,7 +198,7 @@ class Reader():
             l_name = struct.unpack('<i',raw_lname)[0]
             raw_ref_name = self.ubam.read(l_name)
             ref_buffer += raw_ref_name
-            ref_name = raw_ref_name.decode("utf-8")
+            ref_name = raw_ref_name[:-1].decode("utf-8")
             raw_ref_length = self.ubam.read(4)
             ref_buffer += raw_ref_length
             ref_length = struct.unpack('<i',raw_ref_length)[0]
@@ -130,7 +216,7 @@ class Reader():
                 if not self.ubam.read():
                     break
     
-    def __iterator__(self):
+    def __iter__(self):
         return self
     
     def __next__(self):
