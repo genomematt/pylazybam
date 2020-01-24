15890760a9681f7aa3dcdb0a3cdc6874251166da
Matthew Wakefield <matthew.wakefield@wehi.edu.au>
2020-01-24 22:58:08 +1100

    add additional parsing functions and tests

--- pylazybam/bam.py
+++ pylazybam/bam.py
@@ -14,39 +14,19 @@ from array import array
 import re
 from pylazybam.bgzf import BgzfWriter as FileWriter
 
+FLAGS = {'paired' : 0x1,
+         'aligned': 0x2,
+         'unmapped': 0x4,
+         'pair_unmapped': 0x8,
+         'forward' : 0x40,
+         'reverse' : 0x80,
+         'secondary': 0x100,
+         'qc_fail': 0x200,
+         'duplicate': 0x400,
+         'supplementary': 0x800,
+       }
+
 # Parsing functions
-
-def get_flag(alignment):
-    return struct.unpack('<H',alignment[18:20])[0]
-
-def get_len_read_name(alignment):
-    return struct.unpack('<B',alignment[12:13])[0]
-
-def get_read_name(alignment, read_name_length):
-    return alignment[36:36+read_name_length].decode("utf-8")
-
-def get_raw_read_name(alignment, read_name_length):
-    return alignment[36:36+read_name_length]
-
-def get_n_cigar_op(alignment):
-    return struct.unpack('<H',alignment[16:18])[0]
-
-def get_len_sequence(alignment):
-    return struct.unpack('<i',alignment[20:24])[0]
-
-def get_tag_bytes(alignment, len_read_name, n_cigar_op, len_sequence):
-    start = 36 + len_read_name + (4*n_cigar_op) +  (len_sequence+1)//2 + len_sequence
-    return alignment[start:]
-
-def get_raw_seq(alignment, len_read_name, n_cigar_op, len_sequence):
-    start = 36 + len_read_name + (4*n_cigar_op)
-    end = start + (len_sequence+1)//2
-    return alignment[start:end+1]
-
-def get_raw_cigar(alignment,len_read_name, n_cigar_op):
-    start = 36 + len_read_name
-    end = 36 + len_read_name + (4*n_cigar_op)
-    return alignment[start:end+1]
 
 def get_ref_index(alignment):
     return struct.unpack('<i', alignment[4:8])[0]
@@ -54,23 +34,57 @@ def get_ref_index(alignment):
 def get_pos(alignment):
     return struct.unpack('<i', alignment[8:12])[0]
 
+def get_len_read_name(alignment):
+    return struct.unpack('<B',alignment[12:13])[0]
+
 def get_mapq(alignment):
     return struct.unpack('<B', alignment[13:14])[0]
 
-def get_AS(tag_bytes):
-    """A function to extract an AS tag from a raw bam byte string
-    Can be used on a raw alignment entry.
-    Raises ValueError if multiple matches.
-    Recommended try accept for use on raw alignment with fall back
-    to calling on only the tag byte string.
-    """
-    match = re.findall(b'ASC.',tag_bytes)
-    if not match:
-        return None
-    elif len(match) != 1:
-        raise ValueError(f"More than one match to b'ASC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
-    else:
-        return struct.unpack('<xxxB',match[0])[-1]
+def get_bin(alignment):
+    return struct.unpack('<H', alignment[14:16])[0]
+
+def get_number_cigar_opperations(alignment):
+    return struct.unpack('<H',alignment[16:18])[0]
+
+def get_flag(alignment):
+    return struct.unpack('<H',alignment[18:20])[0]
+
+def get_len_sequence(alignment):
+    return struct.unpack('<i',alignment[20:24])[0]
+
+def get_pair_ref_index(alignment):
+    return struct.unpack('<i', alignment[24:28])[0]
+
+def get_pair_pos(alignment):
+    return struct.unpack('<i', alignment[28:32])[0]
+
+def get_template_len(alignment):
+    return struct.unpack('<i', alignment[32:36])[0]
+
+def get_read_name(alignment, read_name_length):
+    return alignment[36:35+read_name_length].decode("utf-8")
+
+def get_raw_read_name(alignment, read_name_length):
+    return alignment[36:36+read_name_length]
+
+def get_raw_cigar(alignment,len_read_name, number_cigar_opperations):
+    start = 36 + len_read_name
+    end = 36 + len_read_name + (4*number_cigar_opperations)
+    return alignment[start:end]
+
+def get_tag_bytestring(alignment, len_read_name, number_cigar_opperations, len_sequence):
+    start = 36 + len_read_name + (4*number_cigar_opperations) +  (len_sequence+1)//2 + len_sequence
+    return alignment[start:]
+
+def get_raw_seq(alignment, len_read_name, number_cigar_opperations, len_sequence):
+    start = 36 + len_read_name + (4*number_cigar_opperations)
+    end = start + (len_sequence+1)//2
+    return alignment[start:end]
+
+def get_raw_base_qual(alignment, len_read_name, number_cigar_opperations, len_sequence):
+    start = 36 + len_read_name + (4*number_cigar_opperations) + (len_sequence+1)//2
+    end = start + len_sequence
+    return alignment[start:end]
 
 def get_AS(tag_bytes):
     """A function to extract an AS tag from a raw bam byte string
@@ -138,10 +152,10 @@ def get_MD(tag_bytes):
     else:
         return match[0][3:-1].decode()
 
-def decode_bam_seq(bamseq):
+def decode_seq(raw_seq):
     bam_bases = '=ACMGRSVTWYHKDBN'
     result = ''
-    for x in array('B', bamseq):
+    for x in array('B', raw_seq):
         result += bam_bases[x >> 4]
         result += bam_bases[x & 0b1111]
     return result
@@ -150,7 +164,12 @@ def decode_cigar(raw_cigar):
     codes = 'MIDNSHP=X'
     cigar = [str(x >> 4) + codes[x & 0b1111] for x in array('I', raw_cigar)]
     return ''.join(cigar)
-   
+
+def decode_base_qual(raw_base_qual, offset=33):
+    return ''.join([chr(q + offset) for q in list(raw_base_qual)])
+
+def is_flag(alignment, flag):
+    return bool(bam.get_flag(a) & flag)
 
 class FileReader():
     """A Pure Python Lazy Bam Parser Class
@@ -158,8 +177,9 @@ class FileReader():
     def __init__(self, ubam):
         self.ubam = ubam
         self.magic = self.ubam.read(4)
-        assert self.magic == b'BAM\x01' ###TODO add more informative errors
-        self.raw_header, self.ascii_header = self.read_header()
+        if self.magic != b'BAM\x01':
+            raise ValueError(f"Incorrect start to uncompressed bam: {self.magic} not b'BAM\x01'. Check the file has been decompressed before passing to this class")
+        self.raw_header, self.header = self.read_header()
         self.raw_refs, self.refs = self.read_refs()
         self.index_to_ref = dict(zip(range(len(self.refs)),self.refs.keys()))
         self.ref_to_index = dict(zip(self.refs.keys(),range(len(self.refs))))
@@ -182,7 +202,7 @@ class FileReader():
         raw_header_length = self.ubam.read(4)
         header_length = struct.unpack('<i',raw_header_length)[0]
         raw_header = raw_header_length+self.ubam.read(header_length)
-        header = raw_header.decode("utf-8")
+        header = raw_header[4:].decode("utf-8")
         return (raw_header, header)
  
     def read_refs(self):
---
+++ pylazybam/tests/test_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+pylazybam/tests/test_all.py
+
+Copyright (c) 2018-2020 Matthew Wakefield, The Walter and Eliza Hall Institute and The University of Melbourne. All rights reserved.
+"""
+
+import unittest
+from pylazybam.tests.test_bam import *
+
+__author__ = "Matthew Wakefield"
+__copyright__ = "Copyright 2018-2020 Matthew Wakefield, The Walter and Eliza Hall Institute and The University of Melbourne"
+__credits__ = ["Matthew Wakefield",]
+__license__ = "BSD-3-Clause"
+__version__ = "0.1.0"
+__maintainer__ = "Matthew Wakefield"
+__email__ = "wakefield@wehi.edu.au"
+__status__ = "Development/Beta"
+
+if __name__ == "__main__":
+    unittest.main()
