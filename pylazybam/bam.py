f01fa3ff6f29201a563734c7963e38e4d0917012
genomematt <matthew.wakefield@wehi.edu.au>
2020-01-27 16:23:39 +1100

    add first pass type hints

--- pylazybam/bam.py
+++ pylazybam/bam.py
@@ -8,10 +8,12 @@ License     : BSD-3-Clause
 Maintainer  : matthew.wakefield@unimelb.edu.au 
 Portability : POSIX
 """
+from __future__ import annotations
 import sys, os
 import struct
 from array import array
 import re
+from typing import BinaryIO, Generator, Tuple, Dict
 from pylazybam.bgzf import BgzfWriter as FileWriter
 
 FLAGS = {'paired' : 0x1,
@@ -28,65 +30,65 @@ FLAGS = {'paired' : 0x1,
 
 # Parsing functions
 
-def get_ref_index(alignment):
+def get_ref_index(alignment: bytes) -> int:
     return struct.unpack('<i', alignment[4:8])[0]
 
-def get_pos(alignment):
+def get_pos(alignment: bytes) -> int:
     return struct.unpack('<i', alignment[8:12])[0]
 
-def get_len_read_name(alignment):
+def get_len_read_name(alignment: bytes) -> int:
     return struct.unpack('<B',alignment[12:13])[0]
 
-def get_mapq(alignment):
+def get_mapq(alignment: bytes) -> int:
     return struct.unpack('<B', alignment[13:14])[0]
 
-def get_bin(alignment):
+def get_bin(alignment: bytes) -> int:
     return struct.unpack('<H', alignment[14:16])[0]
 
-def get_number_cigar_opperations(alignment):
+def get_number_cigar_opperations(alignment: bytes) -> int:
     return struct.unpack('<H',alignment[16:18])[0]
 
-def get_flag(alignment):
+def get_flag(alignment: bytes) -> int:
     return struct.unpack('<H',alignment[18:20])[0]
 
-def get_len_sequence(alignment):
+def get_len_sequence(alignment: bytes) -> int:
     return struct.unpack('<i',alignment[20:24])[0]
 
-def get_pair_ref_index(alignment):
+def get_pair_ref_index(alignment: bytes) -> int:
     return struct.unpack('<i', alignment[24:28])[0]
 
-def get_pair_pos(alignment):
+def get_pair_pos(alignment: bytes) -> int:
     return struct.unpack('<i', alignment[28:32])[0]
 
-def get_template_len(alignment):
+def get_template_len(alignment: bytes) -> int:
     return struct.unpack('<i', alignment[32:36])[0]
 
-def get_read_name(alignment, read_name_length):
+def get_read_name(alignment: bytes, read_name_length: int) -> str:
     return alignment[36:35+read_name_length].decode("utf-8")
 
-def get_raw_read_name(alignment, read_name_length):
+def get_raw_read_name(alignment: bytes, read_name_length: int) -> bytes:
     return alignment[36:36+read_name_length]
 
-def get_raw_cigar(alignment,len_read_name, number_cigar_opperations):
+def get_raw_cigar(alignment: bytes, len_read_name: int, number_cigar_opperations: int) -> bytes:
     start = 36 + len_read_name
     end = 36 + len_read_name + (4*number_cigar_opperations)
     return alignment[start:end]
 
-def get_tag_bytestring(alignment, len_read_name, number_cigar_opperations, len_sequence):
+def get_tag_bytestring(alignment: bytes, len_read_name: int, number_cigar_opperations: int, len_sequence: int) -> bytes:
     start = 36 + len_read_name + (4*number_cigar_opperations) +  (len_sequence+1)//2 + len_sequence
     return alignment[start:]
 
-def get_raw_seq(alignment, len_read_name, number_cigar_opperations, len_sequence):
+def get_raw_seq(alignment: bytes, len_read_name: int, number_cigar_opperations: int, len_sequence: int) -> bytes:
     start = 36 + len_read_name + (4*number_cigar_opperations)
     end = start + (len_sequence+1)//2
     return alignment[start:end]
 
-def get_raw_base_qual(alignment, len_read_name, number_cigar_opperations, len_sequence):
+def get_raw_base_qual(alignment: bytes, len_read_name: int, number_cigar_opperations: int, len_sequence: int) -> bytes:
     start = 36 + len_read_name + (4*number_cigar_opperations) + (len_sequence+1)//2
     end = start + len_sequence
     return alignment[start:end]
 
-def get_AS(tag_bytes):
+def get_AS(tag_bytes: bytes) -> int:
     """A function to extract an AS tag from a raw bam byte string
     Can be used on a raw alignment entry.
     Raises ValueError if multiple matches.
@@ -99,9 +101,9 @@ def get_AS(tag_bytes):
     elif len(match) != 1:
         raise ValueError(f"More than one match to b'ASC.' was found in {tag_bytes} meaning that more than one value of the tag is present or that another tag contains a match as part of its value")
     else:
-        return struct.unpack('<xxxB',match[0])[-1]
+        return struct.unpack('<xxxB',match[0])[-1] #type: int
 
-def get_XS(tag_bytes):
+def get_XS(tag_bytes: bytes) -> int:
     """A function to extract an XS tag from a raw bam byte string
     for genome aligner definition of XS where XS:i:<int> is the
     alignment score of the next best alignment.
@@ -120,7 +122,7 @@ def get_XS(tag_bytes):
     else:
         return struct.unpack('<xxxB',match[0])[-1]
 
-def get_ZS(tag_bytes):
+def get_ZS(tag_bytes: bytes) -> int:
     """A function to extract an ZS tag from a raw bam byte string
     ZS is the equivalent to XS:i:<int> tag in some spliced aligners
     including HISAT2.
@@ -137,7 +139,7 @@ def get_ZS(tag_bytes):
     else:
         return struct.unpack('<xxxB',match[0])[-1]
 
-def get_MD(tag_bytes):
+def get_MD(tag_bytes: bytes) -> str:
     """A function to extract an MD tag from a raw bam byte string
     Can be used on a raw alignment entry.
     Raises ValueError if multiple matches.
@@ -152,7 +154,7 @@ def get_MD(tag_bytes):
     else:
         return match[0][3:-1].decode()
 
-def decode_seq(raw_seq):
+def decode_seq(raw_seq: bytes) -> str:
     bam_bases = '=ACMGRSVTWYHKDBN'
     result = ''
     for x in array('B', raw_seq):
@@ -160,21 +162,23 @@ def decode_seq(raw_seq):
         result += bam_bases[x & 0b1111]
     return result
 
-def decode_cigar(raw_cigar):
+def decode_cigar(raw_cigar: bytes) -> str:
     codes = 'MIDNSHP=X'
     cigar = [str(x >> 4) + codes[x & 0b1111] for x in array('I', raw_cigar)]
     return ''.join(cigar)
 
-def decode_base_qual(raw_base_qual, offset=33):
+def decode_base_qual(raw_base_qual: bytes, offset: int = 33) -> str:
     return ''.join([chr(q + offset) for q in list(raw_base_qual)])
 
-def is_flag(alignment, flag):
+def is_flag(alignment: bytes, flag: int) -> bool:
     return bool(bam.get_flag(a) & flag)
 
 class FileReader():
     """A Pure Python Lazy Bam Parser Class
     """
-    def __init__(self, ubam):
+    def __init__(self, ubam: BinaryIO):
+        """
+        """
         self.ubam = ubam
         self.magic = self.ubam.read(4)
         if self.magic != b'BAM\x01':
@@ -186,7 +190,7 @@ class FileReader():
         self.index_to_ref[-1] = '*'
         self.alignments = self._get_alignments()
 
-    def __enter__(self):
+    def __enter__(self: FileReader) -> FileReader:
         """Open a file operable with WITH statement."""
         return self
     
@@ -198,14 +202,14 @@ class FileReader():
         """Close input file"""
         return self.ubam.close()
 
-    def read_header(self):
+    def read_header(self) -> Tuple[bytes, str]:
         raw_header_length = self.ubam.read(4)
         header_length = struct.unpack('<i',raw_header_length)[0]
         raw_header = raw_header_length+self.ubam.read(header_length)
         header = raw_header[4:].decode("utf-8")
         return (raw_header, header)
  
-    def read_refs(self):
+    def read_refs(self) -> Tuple[bytes, Dict[str,int]]:
         ref_buffer = b""
         refs = {}
 
@@ -225,7 +229,7 @@ class FileReader():
             refs[ref_name] = ref_length
         return (ref_buffer,refs)
     
-    def _get_alignments(self):
+    def _get_alignments(self) -> Generator[bytes]:
         while self.ubam:
             try:
                 raw_blocksize = self.ubam.read(4)
